463132ba7262acca42818a0dbc653610446ec303
Petr Viktorin <encukou@gmail.com>
2016-10-11 19:31:10 +0200

    Ukázková aplikace

--- flapp.py
+++ flapp.py
@@ -1,9 +1,28 @@
-from flask import Flask
+import datetime
+
+from flask import Flask, url_for, render_template
+from jinja2 import Markup
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'MI-PYT je nejlepší předmět na FITu!'
+def index():
+    kus_html = Markup('<b>aho<i>j</i></b>')
+    now = datetime.datetime.now()
+    return render_template(
+        'hello.html',
+        now=now,
+        kus_html=kus_html)
+
+@app.route('/hello/<name>/')
+def hello(name):
+    return render_template('hello.html',
+                           name=name,
+                           count=3)
+
+@app.template_filter('time')
+def human_time(t):
+    return t.strftime("%Y")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=2222)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, port=12843)
