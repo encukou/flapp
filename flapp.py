0b6f7ca741b664815b56409ed60e53c53f502ae3
Petr Viktorin <encukou@gmail.com>
2016-10-11 14:15:17 +0200

    Initial commit

---
+++ flapp.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'MI-PYT je nejlepší předmět na FITu!'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=2222)
